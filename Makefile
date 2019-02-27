c7233d4f682447a25a4874181c5d7728af9ccca6
franksun007 <erpang007@gmail.com>
2019-02-27 00:28:53 -0800

    fixed memory leak

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+
+dbg: main.cc
+	g++-8 -Wall main.cc -g -O0 -lstdc++fs -lcurses -std=c++17 -lgflags -lpthread -o main
+
 all: main.cc
 	g++-8 -Wall main.cc -lstdc++fs -lcurses -std=c++17 -lgflags -lpthread -march=native -O3 -o main
 
--- main.cc
+++ main.cc
@@ -16,6 +16,8 @@
 DEFINE_string(frames, "", "The folder contains all the frames");
 DEFINE_int32(subsample, 2, "Subsamples of each frame");
 DEFINE_int32(fps, 30, "The FPS to play on the terminal");
+DEFINE_int32(acceleration, 1,
+             "The acceleration x you want to apply. More for debug");
 
 namespace {
 
@@ -35,6 +37,7 @@ std::vector<uint8_t> ReadPPM(const std::string &filename) {
   file.ignore(kPPMHeaderSize);
   // TODO(Frank): Unsafe
   file.read((char *)data.data(), kRgbSize);
+  file.close();
 
   return data;
 }
@@ -51,8 +54,6 @@ const std::vector<std::string> GetListOfFiles(const std::string &base_dir) {
 std::string ProcessFrame(const std::vector<uint8_t> data) {
   std::stringstream ss;
   constexpr int32_t kStride = kImageWidth * kColorComponent;
-  int result = 0;
-  setupterm(NULL, STDOUT_FILENO, &result);
   for (int j = 0; j < kImageHeight; j += FLAGS_subsample) {
     for (int i = 0; i < kStride - (kColorComponent * FLAGS_subsample - 1);
          i += kColorComponent * FLAGS_subsample) {
@@ -62,7 +63,7 @@ std::string ProcessFrame(const std::vector<uint8_t> data) {
       }
       sum /= FLAGS_subsample;
 
-      // TODO(Frank): Use switch?
+      // TODO(Frank): Use make switch?
       // TODO(Frank): None linear
       if (sum < 20) {
         ss << "\033[1;31m0";
@@ -79,9 +80,13 @@ std::string ProcessFrame(const std::vector<uint8_t> data) {
 }
 
 void PrintFrame(const std::string &string) {
+  int result = 0;
+  setupterm(NULL, STDOUT_FILENO, &result);
+  putp(tigetstr("clear"));
   printf("%s\n", string.c_str());
   // TODO(Frank): magic number
-  usleep((int32_t)(0.03333333333 * 1000000 * 30 / FLAGS_fps));
+  usleep(
+      (int32_t)(0.03333333333 * 1000000 * 30 / FLAGS_fps / FLAGS_acceleration));
 }
 
 } // namespace
@@ -90,30 +95,19 @@ int main(int argc, char **argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
   const std::vector<std::string> all_filelist = GetListOfFiles(FLAGS_frames);
-  std::vector<std::string> all_frame;
-  all_frame.resize(all_filelist.size());
-  // Served as a threadpool.
-  constexpr int32_t kChunkSize = 32;
-  std::future<std::string> pointer[kChunkSize];
+  std::vector<std::string> all_frame(all_filelist.size());
+  // TODO(Frank): Too much memory. Implement a threadpool.
+  std::vector<std::future<std::string>> handlers(all_filelist.size());
 
-  size_t fl_index = 0;
-  for (; fl_index < all_filelist.size() - (kChunkSize - 1);
-       fl_index += kChunkSize) {
-    // Schedule the work
-    for (int i = 0; i < kChunkSize; i++) {
-      pointer[i] = std::async(std::launch::async, ProcessFrame,
-                              ReadPPM(all_filelist[fl_index + i]));
-    }
-    // Sync
-    for (int i = 0; i < kChunkSize; i++) {
-      all_frame[fl_index + i] = pointer[i].get();
-    }
+  // Schedule the work
+  for (size_t i = 0; i < all_filelist.size(); ++i) {
+    handlers[i] =
+        std::async(std::launch::async, ProcessFrame, ReadPPM(all_filelist[i]));
   }
 
-  // Tail work
-  for (; fl_index < all_filelist.size(); fl_index++) {
-    all_frame[fl_index] =
-        ProcessFrame(ReadPPM(all_filelist[fl_index]));
+  // Sync
+  for (size_t i = 0; i < all_filelist.size(); ++i) {
+    all_frame[i] = handlers[i].get();
   }
 
   // Print Frame
